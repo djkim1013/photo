250c237686edb0cd04dd0315013eb3026a42ad25
djkim <dongjukim1013@gmail.com>
2021-12-02 16:47:01 +0900

    entity set

---
+++ src/main/java/com/example/domain/Folder.java
@@ -0,0 +1,28 @@
+package com.example.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Folder {
+
+    @Id @GeneratedValue
+    @Column(name = "folder_id")
+    private Long id;
+
+    @Column(name = "folder_name")
+    private String name;
+
+    @Column(name = "folder_regdate")
+    private LocalDateTime regdate;
+
+    @OneToMany(mappedBy = "photo")
+    @Column(name = "photo_list")
+    private List<Photo> photos = new ArrayList<>();
+}
