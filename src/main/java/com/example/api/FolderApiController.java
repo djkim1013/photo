7d2aab1a2f0e46c889177616b67db7a27c9a3cc0
djkim <dongjukim1013@gmail.com>
2021-12-06 18:00:22 +0900

    controllers

--- src/main/java/com/example/api/FolderApiController.java
+++ src/main/java/com/example/api/FolderApiController.java
@@ -5,10 +5,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FolderApiController {
     //get
+    //폴더 모두 조회
+    //폴더 이름 조회
 
     //post
+    //폴더 생성
 
     //put
+    //폴더 이름 수정
 
     //del
+    //폴더 삭제
+    //폴더 하위에 있던 사진도 모두 삭제
 }
