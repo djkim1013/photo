b645bcb6a726f3ac6ea73f2e4af76b9aa90609db
djkim <dongjukim1013@gmail.com>
2021-12-06 18:00:22 +0900

    controllers

--- src/main/java/com/example/api/PhotoApiController.java
+++ src/main/java/com/example/api/PhotoApiController.java
@@ -5,10 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PhotoApiController {
     //get
+    //사진 모두 조회
+    //사진 이름으로 조회
+    //사진 날짜로 조회
 
     //post
+    //사진 저장
 
     //put
 
     //del
+    //사진 삭제
 }
