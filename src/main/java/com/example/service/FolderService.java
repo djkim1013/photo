148869be0618cd75cdc8d7e5cdfbe7ce7d05d4c4
djkim <dongjukim1013@gmail.com>
2021-12-06 16:58:47 +0900

    repositories, services

--- src/main/java/com/example/service/FolderService.java
+++ src/main/java/com/example/service/FolderService.java
@@ -8,33 +8,43 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
-@RequiredArgsConstructor    //setter 사용시 데이터 변경을 추적하기 힘드므로, 생성자 사용(lombok)
+@RequiredArgsConstructor    //setter 지양 -> 생성자 사용 ... bean에 추가가능한 타입의 final 필드 변수는 자동으로 의존성이 주입된다.)
 public class FolderService {
 
-    //컴파일 시점에 생성 보장을 위해 final 선언
-    private final FolderRepository folderRepository;
+    private final FolderRepository folderRepository;    //컴파일 시점에서 생성됨을 보장받기 위해 final 선언
     
     //폴더 생성
     @Transactional
-    public Long makeNew(Folder folder){
+    public Long createFolder(Folder folder){
         folderRepository.save(folder);
         return folder.getId();
     }
 
     //모든 폴더 조회
-    public List<Folder> findFolders(){
+    public List<Folder> findAllFolders(){
         return folderRepository.findAll();
     }
 
     //폴더 이름으로 조회
-    //    public List<Folder> findFoldersByName(String name){
-//        return folderRepository.findByName(name);
-//    }
+    public Folder findFolderByName(String name){
+        return folderRepository.findByName(name);
+    }
 
     //폴더 이름 변경
+    @Transactional
+    public void updateFolderName(Long id, String name){
+        Optional<Folder> folder = folderRepository.findById(id);
+        folder.get().setName(name);
+        folderRepository.save(folder.get());
+    }
 
     //폴더 삭제
+    @Transactional
+    public void deleteFolder(Long id){
+        folderRepository.deleteById(id);
+    }
 }
